d775a078979328c7b53084f15220189a1b3aed44
Vlam1r <vvm22@cam.ac.uk>
2022-01-19 13:47:20 +0000

    Extra minor optimisations in low level spi

--- pico_firmware/src/lib/messages/spi_data.c
+++ pico_firmware/src/lib/messages/spi_data.c
@@ -23,8 +23,7 @@ static uint8_t dummy = 0xff;
 ///
 void spi_send_blocking(const uint8_t *data, uint16_t len, uint16_t new_flag) {
     flag = new_flag;
-
-    uint8_t hdr[5] = {dummy, len >> 8, len, flag >> 8, flag};
+    uint8_t hdr[5] = {dummy, len, len >> 8, flag, flag >> 8};
     spi_write_blocking(spi_default, hdr, 5);
     spi_write_blocking(spi_default, data, len);
 }
@@ -36,19 +35,18 @@ void spi_send_blocking(const uint8_t *data, uint16_t len, uint16_t new_flag) {
 ///
 uint16_t spi_receive_blocking(uint8_t *data) {
     uint8_t hdr[4];
-    uint16_t len = 0;
+    uint16_t *len = (uint16_t *) hdr;
 
     do {
         spi_read_blocking(spi_default, 0, &dummy, 1);
     } while (dummy != 0xff);
 
     spi_read_blocking(spi_default, 0, hdr, 4);
-    len = (hdr[0] << 8) | hdr[1];
+#if __IS_MASTER__
+    busy_wait_us(5); // 2us delay is too little, 3us is ok. Going for 5us just to be safe
+#endif
+    spi_read_blocking(spi_default, 0, data, *len);
 
-    for (int i = 0; i < 100; i++) tight_loop_contents();
-
-    spi_read_blocking(spi_default, 0, data, len & 0xFF);
-    flag = (hdr[2] << 8) | hdr[3];
-
-    return len;
+    flag = *(uint16_t *) &hdr[2];
+    return *len;
 }
